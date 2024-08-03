e5f4692beb437125efdcd2f56cf5062a2a65afe0
Hossam Maurice <Eklado@users.noreply.github.com>
2024-08-03 18:34:48 +0300

    fix: adjust phpunit bootstrap file route

--- Splitter/TestDistribution.php
+++ Splitter/TestDistribution.php
@@ -130,7 +130,7 @@ class TestDistribution
         // replace the original "testsuites" and place the new partial one
         $phpunit = $xml->getElementsByTagName('phpunit')->item(0);
 
-        $bootstrap = '~/' . $phpunit->getAttribute('bootstrap');
+        $bootstrap = $this->argumentHandler->getBasePath() . '/' . $phpunit->getAttribute('bootstrap');
         $phpunit->setAttribute('bootstrap', $bootstrap);
 
         $phpunit->replaceChild($testsuites, $phpunit->getElementsByTagName('testsuites')->item(0));
