f764ae55cf436113054d4d874ce656dc32d54aaf
Hossam Maurice <Eklado@users.noreply.github.com>
2025-02-08 14:15:16 +0200

    fix: breaking file

--- phpunit-xml-splitter.php
+++ phpunit-xml-splitter.php
@@ -8,7 +8,7 @@ use Splitter\ArgumentHandler;
 use Splitter\TestDistribution;
 use Splitter\XMLHandler;
 
-echo PHP_EOL . "version: " . getenv('ACTION_REF') . PHP_EOL . PHP_EOL;
+echo PHP_EOL . "version: 1.0.3" . PHP_EOL . PHP_EOL;
 
 // Parse command-line arguments
 $argumentHandler = new ArgumentHandler();
